7c65512abce9b7c233c4f78498aa24e4352bd4e2
TalhaAhmed2001 <talhacool01@gmail.com>
2023-04-16 14:23:09 +0500

    login, register page ui/ux done

---
+++ src/components/RegisterAlumnus.jsx
@@ -0,0 +1,99 @@
+import React,{useState} from 'react'
+
+const RegisterAlumnus = () => {
+
+
+    const [profile, setProfile] = useState({
+        first_name: '',
+        last_name: '',
+        id: '',
+        password: '',
+        sex: '',
+        degree: '',
+        major: '',
+        graduation: ''
+    })
+
+    const { first_name, last_name, id, password, sex, degree, major, graduation } = profile;
+
+    const onChange = (e) => {
+        //console.log("ON CHANGE KE ANDAR")
+        setProfile((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        })
+        )
+    }
+
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setProfile({
+            first_name: '',
+            last_name: '',
+            id: '',
+            password: '',
+            sex: '',
+            degree: '',
+            major: '',
+            graduation: ''
+        })
+    }
+
+  return (
+    <>
+    <form onSubmit={onSubmit}>
+                <div className='form-group'>
+                    <label>
+                        First Name:
+                        <input required type="text" id = 'first_name' name = 'first_name' value={first_name} placeholder='first name' className='form-control' onChange={onChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Last Name:
+                        <input required type="text"  id = 'last_name' name = 'last_name' value={last_name}  placeholder='last name' className='form-control' onChange={onChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Sex:
+                        <select required id = 'sex' name = 'sex' value={sex}  className='form-control' onChange={onChange}>
+                            <option value="">-- Please select --</option>
+                            <option value="M">Male</option>
+                            <option value="F">Female</option>
+                        </select>
+                    </label>
+                    <br />
+                    <label>
+                        ID:
+                        <input required type="number" id = 'id' name = 'id' value={id}  placeholder='id' className='form-control' onChange={onChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Password:
+                        <input required type="password" id = 'password' name = 'password' value={password}  placeholder='password' className='form-control' onChange={onChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Degree:
+                        <input required type='text' id = 'degree' name='degree' value={degree}  placeholder='degree' className='form-control' onChange={onChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Major:
+                        <input required type='text' id = 'degree' name='degree' value={degree}  placeholder='degree' className='form-control' onChange={onChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Graduation:
+                        <input required type='number' id = 'degree' name='degree' value={degree}  placeholder='degree' className='form-control' onChange={onChange} />
+                    </label>
+                    <br />
+                    <button type="submit">Register</button>
+                </div>
+            </form></>
+    
+  )
+
+}
+
+export default RegisterAlumnus
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,82 +1,27 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import RegisterStudent from '../components/RegisterStudent'
+import RegisterAlumnus from '../components/RegisterAlumnus'
 
 const Register = () => {
 
-    const [profile, setProfile] = useState({
-        first_name: '',
-        last_name: '',
-        id: '',
-        password: '',
-        sex: '',
-        degree: ''
-    })
+    const [user, setUser] = useState('student')
 
-    const { first_name, last_name, id, password, sex, degree } = profile;
-
-    const onChange = (e) => {
-        //console.log("ON CHANGE KE ANDAR")
-        setProfile((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value
-        })
-        )
+    const setStudent = (e) => {
+        setUser('student')
     }
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        setProfile({
-            first_name: '',
-            last_name: '',
-            id: '',
-            password: '',
-            sex: '',
-            degree: ''
-        })
+    const setAlumnus = (e) => {
+        setUser('alumnus')
     }
 
     return (
-        <div>
+        <div className='container'>
             <h1>Register Page</h1>
-            <form onSubmit={onSubmit}>
-                <div className='form-group'>
-                    <label>
-                        First Name:
-                        <input required type="text" id = 'first_name' name = 'first_name' value={first_name} placeholder='first name' className='form-control' onChange={onChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Last Name:
-                        <input required type="text"  id = 'last_name' name = 'last_name' value={last_name}  placeholder='last name' className='form-control' onChange={onChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Sex:
-                        <select required id = 'sex' name = 'sex' value={sex}  className='form-control' onChange={onChange}>
-                            <option value="">-- Please select --</option>
-                            <option value="M">Male</option>
-                            <option value="F">Female</option>
-                        </select>
-                    </label>
-                    <br />
-                    <label>
-                        ID:
-                        <input required type="number" id = 'id' name = 'id' value={id}  placeholder='id' className='form-control' onChange={onChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Password:
-                        <input required type="password" id = 'password' name = 'password' value={password}  placeholder='password' className='form-control' onChange={onChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Degree:
-                        <input required type='text' id = 'degree' name='degree' value={degree}  placeholder='degree' className='form-control' onChange={onChange} />
-                    </label>
-                    <br />
-                    <button type="submit">Register</button>
-                </div>
-            </form>
+            
+            <button onClick={setStudent}>Register as Student</button>
+            <button onClick={setAlumnus}>Register as Alumnus</button>
+            {user==='student' ? <RegisterStudent/> : <RegisterAlumnus/>}
             <p>Already have an account? <Link to="/">Login</Link></p>
         </div>
     )
